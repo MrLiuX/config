42fed648410b790f20dadd91a5d493f70f4ed23e
Asher Liu <i@mrliux.com>
2021-08-31 18:34:45 +0800

    .

--- configure.sh
+++ configure.sh
@@ -1,20 +1,42 @@
 #! /usr/bin/env bash
 
+#install
+############################################################
+
+sudo pacman -S exa ranger vim bat neofetch qv2ray
+
+#vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#zsh-plug
+sudo pacman -S zsh-syntax-highlighting zsh-autosuggestions zsh-theme-powerlevel10k
+
+#configure
+############################################################
+
 configDir=`pwd`
 
-rm ~/.gitconfig
-rm ~/.config/neofetch
-rm ~/.config/ranger
-rm ~/.vimrc
-rm ~/.vim/UltiSnips
-rm ~/.Xmodmap
-rm ~/.xinitrc
+#vim
+ln -sf $configDir/vim/vimrc ~/.vimrc
+ln -sf $configDir/vim/UltiSnips ~/.vim/UltiSnips
 
-ln -s $configDir/git/gitconfig ~/.gitconfig
-ln -s $configDir/neofetch ~/.config/neofetch
-ln -s $configDir/ranger ~/.config/ranger
-ln -s $configDir/vim/vimrc ~/.vimrc
-ln -s $configDir/vim/UltiSnips ~/.vim/UltiSnips
-ln -s $configDir/xmodmap/Xmodmap ~/.Xmodmap
-ln -s $configDir/xorg/xinitrc ~/.xinitrc
+#git
+ln -sf $configDir/git/gitconfig ~/.gitconfig
 
+#neofetch
+ln -sf $configDir/neofetch/config.conf ~/.config/neofetch/config.conf
+
+#ranger
+ln -sf $configDir/ranger ~/.config/ranger
+
+#xmodmap
+ln -sf $configDir/xmodmap/Xmodmap ~/.Xmodmap
+
+#xorg
+ln -sf $configDir/xorg/xinitrc ~/.xinitrc
+
+#qv2ray
+ln -sf $configDir/qv2ray/plugins ~/.config/qv2ray/plugins
+ln -sf $configDir/qv2ray/groups.json ~/.config/qv2ray/groups.json
+ln -sf $configDir/qv2ray/Qv2ray.conf ~/.config/qv2ray/Qv2ray.conf
+ln -sf $configDir/qv2ray/routes.json ~/.config/qv2ray/routes.json
