2c9cf469e8ed7cb4288471009051a95469c32714
Asher Liu <i@mrliux.com>
2021-08-31 16:20:05 +0800

    .

--- configure.sh
+++ configure.sh
@@ -12,9 +12,9 @@ rm ~/.xinitrc
 
 ln -s $configDir/git/gitconfig ~/.gitconfig
 ln -s $configDir/neofetch ~/.config/neofetch
-ln -s $config/ranger ~/.config/ranger
-ln -s $config/vim/vimrc ~/.vimrc
-ln -s $config/vim/UltiSnips ~/.vim/UltiSnips
-ln -s $config/xmodmap/Xmodmap ~/.Xmodmap
+ln -s $configDir/ranger ~/.config/ranger
+ln -s $configDir/vim/vimrc ~/.vimrc
+ln -s $configDir/vim/UltiSnips ~/.vim/UltiSnips
+ln -s $configDir/xmodmap/Xmodmap ~/.Xmodmap
 ln -s $configDir/xorg/xinitrc ~/.xinitrc
 
